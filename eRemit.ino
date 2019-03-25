871542c90da69d1bf10d73c08e78215007ab85f8
Tschibo00 <ballhause@gmx.de>
2019-03-25 20:25:02 +0100

    Sample added, several fixes

--- eRemit.ino
+++ eRemit.ino
@@ -2,6 +2,7 @@
 #include <avr/pgmspace.h>
 #include <math.h>
 #include "font.h"
+#include "gong.h"
 
 #define FASTLED_ALLOW_INTERRUPTS 1
 #include <FastLED.h>
@@ -37,7 +38,10 @@ bool timerPaused;    // true=>pause, false=running
 uint8_t displayState;
 uint8_t toneVal=0;
 uint32_t toneDelay=0;
-uint32_t beepDelay=3000;
+uint32_t beepDelay=4096;
+unsigned int samplePos=100000;      // set to some out-of-bound value
+bool playSample=false;
+uint32_t beepDelays[]={0,2048,3084,4096,8192,12200,16384};
 
 void setup() {
   brightness=80;
@@ -93,43 +97,52 @@ void setup() {
   // setup the encoder in multiple instances
   encTimer=new DigiEnc(9,4,-1,599,false,true);
   encMenu=new DigiEnc(9,4,0,3,true,false);
-  encAlarm=new DigiEnc(9,4,0,15,false,false);
+  encAlarm=new DigiEnc(9,4,0,6,false,false);
   encLight=new DigiEnc(9,4,1,255,false,true);
   encColor=new DigiEnc(9,4,0,7,false,true);
   encLight->val=brightness;
+  encAlarm->val=3;
 
   Serial.begin(115200);
 }
 
 ISR(TIMER1_COMPA_vect){//timer 1 interrupt playing samples (8064/sec)
-/*  if (!playing){
-    OCR4D=0;  // Set PWM value on D6
-  }else{
-    OCR4D=0;  // Set PWM value on D6
-  }*/
-
-
-  // beeper stuff
-  playing=false;
-  if (toneDelay&512){
-    if ((toneDelay&4095)>1024){
-      toneVal+=64;
-      playing=true;
+  if (playSample){
+    if (samplePos<gongWavLen){
+      if (samplePos>20)                         // block the display only after the first 20 samples, so it gets refreshed with the 0:00 display while the sample is playing
+        playing=true;
+      else
+        playing=false;
+      OCR4D=pgm_read_byte(gongWav+samplePos);
+      samplePos++;
+    }else{
+      OCR4D=0;    
+      playing=false;
     }
+  }else{
+    // beeper stuff
+    playing=false;
+    if (toneDelay&512){
+      if ((toneDelay&4095)>1024){
+        toneVal+=64;
+        playing=true;
+      }
+    }
+    if (toneDelay>0)
+      toneDelay--;
+  
+    if (playing)
+      OCR4D=toneVal;
+    else
+      OCR4D=0;    
   }
-  if (toneDelay>0)
-    toneDelay--;
-
-  if (playing)
-    OCR4D=toneVal;
-  else
-    OCR4D=0;
 }
 
 ISR(TIMER3_COMPA_vect){ // Timer3 interrupt running the time (1/sec)
   if (!timerPaused){
     if (secLeft==1){
       toneDelay=beepDelay;
+      samplePos=0;
     }
     if (secLeft>0)
       secLeft--;
@@ -167,22 +180,28 @@ void setBrightness(){
   if ((flashspeed==0)||(displayState!=STATE_TIME)){
     FastLED.setBrightness(brightness);
   }else{
-    FastLED.setBrightness(((uint8_t)((sin(0.001f*millis()*flashspeed)+1.0)/2.0*((float)brightness))));
+    FastLED.setBrightness(((uint8_t)(((sin(0.001f*millis()*flashspeed)*0.45)+0.55)*((float)brightness))));    // don't fade down to black, but only to very dark, so the displayed number is always visible
   }
 }
 
-void drawLogo(const uint16_t *logo, CRGB color){
+void drawLogo(const uint16_t *logo, CRGB color,bool overlay){
   uint16_t c;
   for (uint8_t y=0;y<7;y++){
     c=logo[y];
     for (uint8_t x=0;x<11;x++){
       if (c&0x8000)
         screen[y*11+x]=color;
-      else
-        screen[y*11+x]=CRGB::Black;
+      else{
+        if (!overlay)                     // only paint background black if overlay is not selected
+          screen[y*11+x]=CRGB::Black;
+      }
       c=c<<1;
     }
   }
+}
+
+void drawLogo(const uint16_t *logo, CRGB color){
+  drawLogo(logo, color, false);
 }
 
 void drawDigit(const uint8_t *font, uint8_t pos, uint8_t width, uint8_t number, CRGB color){
@@ -199,7 +218,7 @@ void drawDigit(const uint8_t *font, uint8_t pos, uint8_t width, uint8_t number, 
   }
 }
 
-void drawNumber(int number, CRGB color){
+void drawNumber(int number, CRGB color, bool showTimeSig){
   uint8_t digits;
   uint8_t d0, d1, d2;
 
@@ -223,9 +242,15 @@ void drawNumber(int number, CRGB color){
     drawDigit(font37, 0, 3, d0, color);
     drawDigit(font37, 4, 3, d1, color);
     drawDigit(font37, 8, 3, d2, color);
-    screen[25]=CRGB(color.g/4,color.g/4,color.g/4);
-    screen[47]=CRGB(color.g/4,color.g/4,color.g/4);
+    if (showTimeSig){
+      screen[25]=CRGB(color.g/4,color.g/4,color.g/4);
+      screen[47]=CRGB(color.g/4,color.g/4,color.g/4);
+    }
   }
+}
+
+void drawNumber(int number, CRGB color){
+  drawNumber(number, color, true);
 }
 
 void drawTime(int minutes, CRGB color){
@@ -249,7 +274,7 @@ void drawTime(){
       c=CRGB(0,255,0);
     else {
       if (secLeft<60){
-        flashspeed=10;
+        flashspeed=13;
         c=CRGB(255,0,0);
       }else
         c=CRGB(315-secLeft,secLeft-60,0);
@@ -301,16 +326,24 @@ void loop() {
       
     case STATE_MENU_ALARM_SET:
       if (encAlarm->process()){
-        beepDelay=encAlarm->val*1000;
-        toneDelay=beepDelay;          // play the beep
+        if (encAlarm->val==0){
+          playSample=true;
+          samplePos=0;
+        }else{
+          playSample=false;
+          beepDelay=beepDelays[encAlarm->val];
+          toneDelay=beepDelay;          // play the beep
+        }
       }
+      drawNumber(encAlarm->val,CRGB::White);
+      drawLogo(logos+28, CRGB::Turquoise, true);
       if (getButtonClick())
         displayState=STATE_TIME;
       break;
     case STATE_MENU_LIGHT_SET:
       encLight->process();
       brightness=encLight->val;
-      drawLogo(logos+14, CRGB::White);
+      drawNumber(brightness,CRGB::White, false);
       if (getButtonClick())
         displayState=STATE_TIME;
       break;
